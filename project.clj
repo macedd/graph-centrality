f737096f37aba0f0a5592046b92c7e7eeb2cc206
Thiago Macedo <thiago@internetbudi.com.br>
2024-09-18 03:48:59 -0500

    upgrading lein ring plugin

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot graph-centrality.core
-  :plugins [[lein-ring "0.8.11"]]
+  :plugins [[lein-ring "0.12.6"]]
   :ring {:handler graph-centrality.api/handler}
   :dependencies [
                 [org.clojure/clojure "1.7.0"]
                 [org.clojure/tools.namespace "0.2.11"]
-                [liberator "0.14.1"]
+                [liberator "0.15.3"]
                 [compojure "1.3.4"]
-                [ring/ring-core "1.2.1"]
+                [ring/ring-core "1.12.2"]
                 [cheshire "5.6.0"]])
